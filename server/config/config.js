47c908e5345f22c1767707369298751d6e634c2c
marcusxart <cemeji64@gmail.com>
2024-04-03 01:31:51 +0100

    fix 1

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,20 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+    ssl: true,
   },
   test: {
     username: process.env.DB_USERNAME,
