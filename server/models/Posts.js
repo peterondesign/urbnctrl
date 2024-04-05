9bd21f9054c1fbf65be8e984ad8e8b266af8b979
Kerry kaiso <kerryesua9@gmail.com>
2024-04-05 07:23:57 +0100

    fixes

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -3,19 +3,23 @@ module.exports=(sequelize, DataTypes)=>{
  const Posts=sequelize.define("Posts",{
    title:{
     type: DataTypes.STRING,
-    allowNull : true
+    allowNull : false
    },
     category:{
       type: DataTypes.STRING, 
-      allowNull : true
+      allowNull : false
     },
     author:{
       type: DataTypes.STRING,
-      allowNull : true
+      allowNull : false
     },
    content:{
     type: DataTypes.STRING,
-    allowNull : true
+    allowNull : false
+   },
+   img:{
+    type: DataTypes.STRING,
+    allowNull : false
    }
  })
   
