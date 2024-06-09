d5ee27ec4641cfc70c51b67b2738a65b4d621074
marcusxart <cemeji64@gmail.com>
2024-06-09 23:12:42 +0100

    update

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,37 +1,37 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Home from './pages/home';
-import Utilities from './pages/utilities';
-import AboutUs from './pages/about-us';
-import Community from './pages/community';
-import CommunityPost from './pages/community-post';
-import SignIn from './pages/auth/signIn';
-import SignUp from './pages/auth/signUp';
-import ForgottenPassword from './pages/auth/forgottenPassword';
-import CreatePost from './pages/create-post';
-import Socials from './pages/socials';
-import CreateEvent from './pages/create-event';
-import Announcement from './pages/announcement';
-import PreviewPost from './pages/preview-post';
-import ProtectedRoutes from './components/protectedRoutes';
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
+import Utilities from "./pages/utilities";
+import AboutUs from "./pages/about-us";
+import Community from "./pages/community";
+import CommunityPost from "./pages/community-post";
+import SignIn from "./pages/auth/signIn";
+import SignUp from "./pages/auth/signUp";
+import ForgottenPassword from "./pages/auth/forgottenPassword";
+import CreatePost from "./pages/create-post";
+import Socials from "./pages/socials";
+import CreateEvent from "./pages/create-event";
+import Announcement from "./pages/announcement";
+import PreviewPost from "./pages/preview-post";
+import ProtectedRoutes from "./components/protectedRoutes";
 
 const routes = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: 'utilities', element: <Utilities /> },
-  { path: 'about-us', element: <AboutUs /> },
-  { path: 'announcements', element: <Announcement /> },
+  { path: "/", element: <Home /> },
+  { path: "utilities", element: <Utilities /> },
+  { path: "about-us", element: <AboutUs /> },
+  { path: "announcements", element: <Announcement /> },
   {
-    path: 'community',
+    path: "community",
     children: [
       { index: true, element: <Community /> },
-      { path: ':postId', element: <CommunityPost /> },
+      { path: ":postId", element: <CommunityPost /> },
       {
         element: <ProtectedRoutes />,
         children: [
           {
-            path: 'create-post',
+            path: "create-post",
             children: [
               { index: true, element: <CreatePost /> },
-              { path: 'preview', element: <PreviewPost /> },
+              { path: "preview", element: <PreviewPost /> },
             ],
           },
         ],
@@ -39,19 +39,19 @@ const routes = createBrowserRouter([
     ],
   },
   {
-    path: 'socials',
+    path: "socials",
     children: [
       { index: true, element: <Socials /> },
-      { path: 'create-event', element: <CreateEvent /> },
+      { path: "create-event", element: <CreateEvent /> },
     ],
   },
   {
-    path: 'auth',
+    path: "auth",
     children: [
       { index: true, element: <SignIn /> },
-      { path: 'sign-in', element: <SignIn /> },
-      { path: 'sign-up', element: <SignUp /> },
-      { path: 'forgotten-password', element: <ForgottenPassword /> },
+      { path: "sign-in", element: <SignIn /> },
+      { path: "sign-up", element: <SignUp /> },
+      { path: "forgotten-password", element: <ForgottenPassword /> },
     ],
   },
 ]);
