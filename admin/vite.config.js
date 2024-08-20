65d6301c8a83923cc0b7f0336cda928b566f3619
marcusxart <cemeji64@gmail.com>
2024-08-20 19:23:50 +0100

    added admin

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
 import react from '@vitejs/plugin-react'
-=======
-import react from '@vitejs/plugin-react-swc'
->>>>>>> 4c4fe89a9784528be87655817253616c2dc62f3c
 
 // https://vitejs.dev/config/
 export default defineConfig({
